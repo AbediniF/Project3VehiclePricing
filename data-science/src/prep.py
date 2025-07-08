f140200f9259f78bc1c06f37726524de378029e2
Fereshteh Abedini <149391413+AbediniF@users.noreply.github.com>
2025-07-08 16:44:22 -0400

    Update prep.py

--- data-science/src/prep.py
+++ data-science/src/prep.py
@@ -15,11 +15,11 @@ import mlflow
 def parse_args():
     '''Parse input arguments'''
 
-    parser = argparse.__________("prep")  # Create an ArgumentParser object
-    parser.add_argument("--raw_data", type=_____, help="Path to raw data")  # Specify the type for raw data (str)
-    parser.add_argument("--train_data", type=_____, help="Path to train dataset")  # Specify the type for train data (str)
-    parser.add_argument("--test_data", type=_____, help="Path to test dataset")  # Specify the type for test data (str)
-    parser.add_argument("--test_train_ratio", type=______, default=_____, help="Test-train ratio")  # Specify the type (float) and default value (0.2) for test-train ratio
+    parser = argparse.ArgumentParser("prep")  # Create an ArgumentParser object
+    parser.add_argument("--raw_data", type=str, help="Path to raw data")  # Specify the type for raw data (str)
+    parser.add_argument("--train_data", type=str, help="Path to train dataset")  # Specify the type for train data (str)
+    parser.add_argument("--test_data", type=str, help="Path to test dataset")  # Specify the type for test data (str)
+    parser.add_argument("--test_train_ratio", type=float, default=0.2, help="Test-train ratio")  # Specify the type (float) and default value (0.2) for test-train ratio
     args = parser.parse_args()
 
     return args
@@ -32,10 +32,20 @@ def main(args):  # Write the function name for the main data preparation logic
 
     # ------- WRITE YOUR CODE HERE -------
 
-    # Step 1: Perform label encoding to convert categorical features into numerical values for model compatibility.  
+    # Step 1: Perform label encoding to convert categorical features into numerical values for model compatibility. 
+    le = LabelEncoder()
+    df['Segment'] = le.fit_transform(df['Segment']) 
     # Step 2: Split the dataset into training and testing sets using train_test_split with specified test size and random state.  
+    train_df, test_df = train_test_split(df, test_size=args.test_train_ratio, random_state=42)
     # Step 3: Save the training and testing datasets as CSV files in separate directories for easier access and organization.  
+    # create directories to save train and test datasets
+    os.makedirs(args.train_dataset, exist_ok=True)
+    os.makedirs(args.test_dataset, exist_ok=True)
+    train_df.to_csv(os.path.join(args.train_dataset,"train_dataset.csv"), index=False)
+    test_df.to_csv(os.path.join(args.test_dataset,"test_dataset.csv"), index=False)
     # Step 4: Log the number of rows in the training and testing datasets as metrics for tracking and evaluation.  
+    mlflow.log_metric('train size', train_df.shape[0])
+    mlflow.log_metric('test size', test_df.shape[0])
 
 
 if __name__ == "__main__":
