8daa50f9beeb463ca1529fe538e16717cfb3de36
Fereshteh Abedini <149391413+AbediniF@users.noreply.github.com>
2025-07-09 16:21:01 -0400

    Update register.py

--- data-science/src/register.py
+++ data-science/src/register.py
@@ -19,6 +19,9 @@ def parse_args():
     parser.add_argument("--model_info_output_path", type=str, help="Path to write model info JSON")  # Hint: Specify the type for model_info_output_path (str)
     args, _ = parser.parse_known_args()
     print(f'Arguments: {args}')
+    print(f"Does model_output exist? {os.path.exists(args.model_output)}")
+    print(f"List dir at model_output: {os.listdir(args.model_output) if os.path.exists(args.model_output) else 'N/A'}")
+
 
     return args
     
